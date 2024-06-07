f3ab1f954fb4f2657980c4deff743b99240e58d9
ToavinaMaria <ToavinaMaria>
2024-06-07 05:15:38 +0300

    feat: implements test AssertionTest

--- src/test/java/assertion/AssertionTest.java
+++ src/test/java/assertion/AssertionTest.java
@@ -1,4 +1,21 @@
 package assertion;
 
+import org.example.Assertion;
+import org.example.Evaluator;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AssertionTest {
+    @Test
+    void affirmation_assetion(){
+        var assertion = new Assertion(
+                "Le ciel est rouge"
+        );
+        var evaluator = new Evaluator();
+        Boolean result = evaluator.evaluate(assertion);
+        assertEquals(false,result);
+    }
 }
