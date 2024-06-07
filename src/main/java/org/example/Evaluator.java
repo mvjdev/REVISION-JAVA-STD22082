93c713d6f81aa5003de8affdb477b477450188d5
ToavinaMaria <ToavinaMaria>
2024-06-07 05:14:09 +0300

    feat: implements evaluator

--- src/main/java/org/example/Evaluator.java
+++ src/main/java/org/example/Evaluator.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
 public class Evaluator {
+    private Map<String,Boolean> knowledgeBase;
+
+    public Evaluator() {
+        knowledgeBase = new HashMap<>();
+        knowledgeBase.put("Le ciel est bleu",true);
+        knowledgeBase.put("Le ciel est rouge",false);
+    }
+
+    public Boolean evaluate(Assertion assertion) {
+        String statement = assertion.getStatement();
+        return knowledgeBase.getOrDefault(statement,null);
+    }
 }
